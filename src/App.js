90f4322b5c900895aabbf30aab68d99d081f5ade
bnk22gzq <bnk22gzq@bangor.ac.uk>
2023-06-15 16:10:12 +0100

    changes

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 
 import Navbar from './component/navbar';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { BrowserRouter as Router, Routes, Route,witch } from 'react-router-dom';
 import Home from './screens/Home';
 import Login from './screens/Login/Login.js';
 import Footer from './component/Footer/Footer.js';
@@ -14,8 +14,8 @@ return (
 			<Router>
 				<Navbar />
 			<Routes>
-				<Route path='/Home' element={<Home/>} />
-				<Route path='/Login' element={<Login />} />
+				<Route exact path='/photo-gallary/Home' element={<Home/>} />
+				<Route path='/photo-gallary/login' element={<Login />} />
 			</Routes>
 				<Footer/>
 			</Router>
