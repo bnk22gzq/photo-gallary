8d23abcdf6bdaa6704f46f1b26a6524c5008a8c4
bnk22gzq <bnk22gzq@bangor.ac.uk>
2023-06-15 22:46:05 +0100

    solve 404 github error

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ function App() {
 	
 return (
 	
-			<BrowserRouter>
+			<BrowserRouter basename='/photo-gallary'>
 				<Navbar />
 				<Routes>
 					<Route path="*" element={<Navigate to ="/home" />}/>
